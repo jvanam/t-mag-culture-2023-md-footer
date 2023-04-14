e8616935a8e1a0d22bc952ecb712e94606671b5e
Jess Vanam <32110513+jvanam@users.noreply.github.com>
2023-04-14 09:27:25 -0400

    build error updates

--- src/footer.svelte
+++ src/footer.svelte
@@ -1,7 +1,10 @@
 <section class="wrapper">
 	<a href="https://www.nytimes.com/section/t-magazine">
 		<div class="home-link">
-			<img src="/handwriting-white.png" />
+			<img
+				src="/handwriting-white.png"
+				alt="Handwritten title of the magazine issue, reads 'This Woman's Work'"
+			/>
 		</div>
 	</a>
 	<div class="section-links-wrapper">
